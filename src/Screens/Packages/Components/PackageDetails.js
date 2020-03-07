0b704c9eca9e51eacc1553e618604b5f5994ab24
sohaibusmani <sohaibusmani52@gmail.com>
2020-03-06 18:22:32 -0800

    Package Details Commit 2

--- src/Screens/Packages/Components/PackageDetails.js
+++ src/Screens/Packages/Components/PackageDetails.js
@@ -1,10 +1,26 @@
 import React , {Component} from 'react';
 
+// Components
+
+import Navbar from '../../../Components/Navbar/Navbar';
+
+// Assets
+
+import logo from '../../../Assets/Images/event1.jpg'
+
 class PackageDetails extends Component {
     render(){
         return(
             <React.Fragment>
-                <h1>Package Details</h1>
+                <Navbar/>
+                <div>
+                <div style={{backgroundImage:`url(${logo})`,
+             width: '100%',
+             height: '400px',
+             backgroundSize:'100% 100%'}}>
+
+              </div>
+                </div>
             </React.Fragment>
         )
     }
