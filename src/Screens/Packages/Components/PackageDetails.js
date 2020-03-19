9fc1c69edea4740b9e3f7b642fba03ffe74b5827
sohaibusmani <sohaibusmani52@gmail.com>
2020-03-19 15:29:12 -0700

    Form added in Package Details

--- src/Screens/Packages/Components/PackageDetails.js
+++ src/Screens/Packages/Components/PackageDetails.js
@@ -1,7 +1,8 @@
 import React , {Component} from 'react';
-import {Card,Col,Row, Container} from 'react-bootstrap';
+import {Card,Col,Row, Container,Form,Button} from 'react-bootstrap';
 import 'react-image-gallery/styles/css/image-gallery.css';
 import ImageGallery from "react-image-gallery";
+
 
 // Components
 
@@ -76,7 +77,28 @@ class PackageDetails extends Component {
                               </Card>
                           </Row>
                           <Row>
-                            
+                            <Col md={12} style={{marginTop:'5rem'}}>
+                            <Form>
+  <Form.Group controlId="formBasicEmail">
+    <Form.Label>Email address</Form.Label>
+    <Form.Control type="email" placeholder="Enter email" />
+    <Form.Text className="text-muted">
+      We'll never share your email with anyone else.
+    </Form.Text>
+  </Form.Group>
+
+  <Form.Group controlId="formBasicPassword">
+    <Form.Label>Password</Form.Label>
+    <Form.Control type="password" placeholder="Password" />
+  </Form.Group>
+  <Form.Group controlId="formBasicCheckbox">
+    <Form.Check type="checkbox" label="Check me out" />
+  </Form.Group>
+  <Button variant="primary" type="submit">
+    Submit
+  </Button>
+</Form>
+                            </Col>
                           </Row>
                         </Col>
                     </Row>
