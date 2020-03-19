b934b7e6e0af4b9c5d6dcae9b841c38dd5920f27
sohaibusmani <sohaibusmani52@gmail.com>
2020-03-19 14:28:20 -0700

    Package Details commit

--- src/Screens/Packages/Components/PackageDetails.js
+++ src/Screens/Packages/Components/PackageDetails.js
@@ -1,4 +1,7 @@
 import React , {Component} from 'react';
+import {Card,Col,Row, Container} from 'react-bootstrap';
+import 'react-image-gallery/styles/css/image-gallery.css';
+import ImageGallery from "react-image-gallery";
 
 // Components
 
@@ -6,10 +9,33 @@ import Navbar from '../../../Components/Navbar/Navbar';
 
 // Assets
 
-import logo from '../../../Assets/Images/event1.jpg'
+import logo from '../../../Assets/Images/event1.jpg';
+import image1 from '../../../Assets/Images/event.jpg';
+import image2 from '../../../Assets/Images/eventsCover.jpg';
+import image3 from '../../../Assets/Images/testimonial.jpg';
 
 class PackageDetails extends Component {
     render(){
+
+        const images = [
+            {
+                original: logo,
+                thumbnail: logo,
+            },
+            {
+                original: image1,
+                thumbnail: image1,
+            },
+            {
+                original: image2,
+                thumbnail: image2,
+            },
+            {
+                original: image3,
+                thumbnail: image3,
+            },
+        ]
+
         return(
             <React.Fragment>
                 <Navbar/>
@@ -20,7 +46,23 @@ class PackageDetails extends Component {
              backgroundSize:'100% 100%'}}>
 
               </div>
+             
                 </div>
+                <Container style={{marginTop:'50px'}} fluid>
+                    <Row>
+                        <Col md={8}>
+                            <div>
+                          <ImageGallery style={{ height: 50 }} thumbnailPosition='bottom' autoPlay={true} items={images}/>
+                          </div>
+                        </Col>
+                        <Col md={4}>
+                          <Row>
+                              
+                          </Row>
+                        </Col>
+                    </Row>
+                </Container>
+                
             </React.Fragment>
         )
     }
