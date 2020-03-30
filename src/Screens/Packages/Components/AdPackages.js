ba9b8134631b71cb49babedd31f8997fe6e1628b
sohaibusmani <sohaibusmani52@gmail.com>
2020-03-30 16:18:41 -0700

    Card Updated

--- src/Screens/Packages/Components/AdPackages.js
+++ src/Screens/Packages/Components/AdPackages.js
@@ -1,12 +1,12 @@
 import React from 'react';
-import  {Card, Container , Row , Col } from 'react-bootstrap';
+import  {Card, Container , Row , Col , Button} from 'react-bootstrap';
 import {Typography} from '@material-ui/core';
 
 // Components
 import Navbar from '../../../Components/Navbar/Navbar'
 
 // Assets
-import logo from '../../../Assets/Images/eventsCover.jpg'
+import logo from '../../../Assets/Images/event1.jpg'
 
 class AdPackages extends React.Component{
     render(){
@@ -14,10 +14,13 @@ class AdPackages extends React.Component{
             <React.Fragment>
                  <Navbar/>
                  <div>
-                     <Card style={{ width: '50rem' }}>
+                     <Card style={{ width: '40rem' , boxShadow:'2px 2px 2px 2px #AD365C'}}>
+                         <Card.Header style={{backgroundColor:'white', color:'#AD365C'}}>
                          <Card.Title>
                              Mehendi Packages
                          </Card.Title>
+                         </Card.Header>
+                         <Card.Body>
                          <Container>
                            <Row>
                                <Col lg={8} md={8} sm={12}>
@@ -26,17 +29,27 @@ class AdPackages extends React.Component{
                                <Col lg={4} md={4} >
                                    <h4>Deal includes:</h4>
                                    <ul>
-                                          <li><Typography>Table décor </Typography></li>
-                                          <li><Typography>Chocolate fountain with Goodies</Typography></li>
-                                          <li><Typography>Juice corner</Typography></li>
-                                          <li><Typography>Balloon décor</Typography></li>
+                                          <li><Typography>Food</Typography></li>
+                                          <li><Typography>Decor</Typography></li>
+                                          <li><Typography>Photography</Typography></li>
+                                          <li><Typography>DJ</Typography></li>
                                          
-                                          <li><Typography>30 floral tiaras</Typography></li>
-                                          <li><Typography>3 bridal shower games</Typography></li>
+                                          <li><Typography>Starter</Typography></li>
+                                          <li><Typography>Games</Typography></li>
                                       </ul>
                                </Col>
                            </Row>
+                           <hr/>
+                           <Row>
+                               <Col lg={3} md={3}>
+                               </Col>
+                               <Col >
+                                 <Button style={{backgroundColor:'white', color:'#AD365C', borderColor:'#AD365C'}} block>View Details</Button>
+                               </Col>
+                               <Col></Col>
+                           </Row>
                          </Container>
+                         </Card.Body>
                      </Card>
                  </div>
             </React.Fragment>
