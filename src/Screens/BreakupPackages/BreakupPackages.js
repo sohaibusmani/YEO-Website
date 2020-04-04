2c688b1e38473edf615730ed32357932edf781d7
sohaibusmani <sohaibusmani52@gmail.com>
2020-04-04 11:51:47 -0700

    Breakup Packages Class created

---
+++ src/Screens/BreakupPackages/BreakupPackages.js
@@ -0,0 +1,13 @@
+import React , {Component} from 'react';
+
+class BreakupPackages extends Component {
+    render(){
+        return(
+            <React.Fragment>
+                <h1>Breakup Packages</h1>
+            </React.Fragment>
+        )
+    }
+}
+
+export default BreakupPackages;
