9b87132f41e6961f356eb6ec430d0443fca129fc
sohaibusmani <sohaibusmani52@gmail.com>
2020-04-08 18:19:46 -0700

    About Work started

--- src/Screens/About/About.js
+++ src/Screens/About/About.js
@@ -1,10 +1,30 @@
 import React , {Component} from 'react';
+import {Container, Row , Col} from 'react-bootstrap';
+
+// Assets
+import us16 from "../../Assets/Images/us5.jpg";
+
+// Components
+import Navbar from '../../Components/Navbar/Navbar';
+import Footer from '../../Components/Footer/Footer'
 
 class About extends Component {
     render(){
         return(
             <React.Fragment>
-                <h1>About</h1>
+                <Navbar/>
+                <Container>
+                    <div style={{
+                        backgroundImage: `url(${us16})`,
+                        width: '100%',
+                        height: '350px',
+                        backgroundSize: '100% 100%',
+                        
+                    }}>
+                        
+
+                    </div>
+                    </Container>
             </React.Fragment>
         )
     }
