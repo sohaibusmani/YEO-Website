ffcd1eb73dc393d12df20382553675b3e108ff8e
sohaibusmani <50169776+sohaibusmani@users.noreply.github.com>
2020-09-05 02:58:21 -0700

    break up package UI commit 1

--- src/Screens/BreakupPackages/BreakupPackages.js
+++ src/Screens/BreakupPackages/BreakupPackages.js
@@ -1,10 +1,22 @@
 import React , {Component} from 'react';
+import {Container } from '@material-ui/core';
+
+import pcover from '../../Assets/Images/us14.jpg';
 
 class BreakupPackages extends Component {
     render(){
         return(
             <React.Fragment>
-                <h1>Breakup Packages</h1>
+                <Container>
+                    <div style={{
+                        backgroundImage: `url(${pcover})`,
+                        width: '100%',
+                        height: '350px',
+                        backgroundSize: '100% 100%',
+
+                    }}>
+                             </div>
+                </Container>
             </React.Fragment>
         )
     }
