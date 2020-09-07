dc3e72d4a0187fe8a1fea2f8cd2ec59bff023c7a
sohaibusmani <50169776+sohaibusmani@users.noreply.github.com>
2020-09-07 01:18:58 -0700

    breakup package UI commit 3

--- src/Screens/BreakupPackages/BreakupPackages.js
+++ src/Screens/BreakupPackages/BreakupPackages.js
@@ -1,10 +1,22 @@
 import React , {Component} from 'react';
-import {Container, Grid} from '@material-ui/core'; 
+import {Container, Grid, withStyles} from '@material-ui/core'; 
 
 import pcover from '../../Assets/Images/us14.jpg';
 
+const styles = theme => ({
+    card: {
+        backgroundColor: '#f5f5f5',
+        padding: 10,
+        [theme.breakpoints.up('sm')]: {
+            padding: '20px 50px 20px',
+        },
+        boxShadow: '0 1px 3px 0 rgba(0, 0, 0, 0.2), 0 2px 5px 0 rgba(0, 0, 0, 0.19)',
+    },
+})
+
 class BreakupPackages extends Component {
     render(){
+        const {classes} = this.props;
         return(
             <React.Fragment>
                 <Container>
@@ -18,14 +30,16 @@ class BreakupPackages extends Component {
                              </div>
                 </Container>
                 <Container maxWidth='lg'>
+                    <div className={classes.card}>
                    <Grid md={8}>
                      <h3>Create Your Own Package</h3>
                      <hr/>
                    </Grid>
+                   </div>
                 </Container>
             </React.Fragment>
         )
     }
 }
 
-export default BreakupPackages;
+export default withStyles(styles)(BreakupPackages);
