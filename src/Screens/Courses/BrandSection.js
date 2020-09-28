f8fd0ec3c08b9f8435f942dc9cdc9d801ff6dd2d
sohaibusmani <50169776+sohaibusmani@users.noreply.github.com>
2020-09-28 14:22:24 +0500

    brand section responsive

--- src/Screens/Courses/BrandSection.js
+++ src/Screens/Courses/BrandSection.js
@@ -1,6 +1,6 @@
 import React , {Component} from 'react';
-import {Container,Row,Card,Col} from 'react-bootstrap';
-import {Typography, Grid} from '@material-ui/core';
+import {Card} from 'react-bootstrap';
+import {Typography,Container, Grid} from '@material-ui/core';
 
 // Components
 import Footer from '../../Components/Footer/Footer';
@@ -38,21 +38,21 @@ class BrandSection extends Component {
               </Container>
                 <Container >
                     
-                    <Row>
-                        <Col xs={12} style={{marginTop:'80px',  fontSize:'20px', textAlign:'center', color:'black', fontFamily:'cursive'}}>
+                    <Grid container spacing={3} >
+                        <Grid xs={12} style={{marginTop:'80px',  fontSize:'20px', textAlign:'center', Gridor:'black', fontFamily:'cursive'}}>
                             <p>
                             Youth event organization is here to give platform to the youth to start their event management business in this market. Youth event organizer start their event management course at Usman Institute of Technology. Their first batch is enrolled and get the knowledge that how any event work. In this course we provide them a platform for their practical consideration so that students can groom their skills and get success in future.  
                             </p>
                         
-                        </Col>
-                    </Row>
-                    <Row style={{marginTop:'20px'}}>
-                        <Col xs={4} md={4}></Col>
-                        <Col>
+                        </Grid>
+                    </Grid>
+                    <Grid container style={{marginTop:'20px'}}>
+                        <Grid item xs={4} lg={4}></Grid>
+                        <Grid>
                             <Typography
                             style={{
                               fontSize: '30px',
-                              color: 'black',
+                              Gridor: 'black',
                               fontFamily:'TimesNewRoman',
                               opacity:'1.9',
                               textAlign: 'left',
@@ -61,79 +61,79 @@ class BrandSection extends Component {
                               lineHeight: '30px',
                               
                           }} variant="h5" component="h2">Our Brands</Typography>
-                        </Col>
-                    </Row>
-                    <Row style={{marginTop:'100px'}}>
-                       <Col md={4}>
-                       <Card style={{ width: '20rem' }}>
+                        </Grid>
+                    </Grid>
+                    <Grid container spacing={3} style={{marginTop:'100px'}}>
+                       <Grid item xs={12} lg={4}>
+                       <Card >
   <Card.Img variant="top" src={bliss} />
-  <Card.Body>
-    <Card.Title style={{color:'#AD365C'}}>The Bliss </Card.Title>
+  <Card.Body style={{textAlign:'center'}}>
+    <Card.Title style={{Gridor:'#AD365C'}}>The Bliss </Card.Title>
     <Card.Text>
     This popular event course guides you through each step so you become a certified event planner!
     </Card.Text>
   </Card.Body>
 </Card>
-                       </Col>
-                       <Col md={4}>
-                       <Card style={{ width: '20rem' }}>
+                       </Grid>
+                       <Grid item xs={12} lg={4}>
+                       <Card>
   <Card.Img variant="top" src={says} />
-  <Card.Body>
-    <Card.Title style={{color:'#AD365C'}}>The Says  </Card.Title>
+  <Card.Body style={{textAlign:'center'}}>
+    <Card.Title style={{Gridor:'#AD365C'}}>The Says  </Card.Title>
     <Card.Text>
     This popular event course guides you through each step so you become a certified event planner!
     </Card.Text>
   </Card.Body>
 </Card>
-                       </Col>
-                       <Col md={4}>
-                       <Card style={{ width: '20rem' }}>
+                       </Grid>
+                       <Grid item xs={12} lg={4}>
+                       <Card >
   <Card.Img variant="top" src={axu} />
-  <Card.Body>
-    <Card.Title style={{color:'#AD365C'}}>The AXU Eventors </Card.Title>
+  <Card.Body style={{textAlign:'center'}}>
+    <Card.Title style={{Gridor:'#AD365C'}}>The AXU Eventors </Card.Title>
     <Card.Text>
     This popular event course guides you through each step so you become a certified event planner!
     </Card.Text>
   </Card.Body>
 </Card>
-                       </Col>
+                       </Grid>
 
-                    </Row>
-                    <Row style={{marginTop:'30px'}}>
-                    <Col md={4}>
-                       <Card style={{ width: '20rem' }}>
+                    </Grid>
+                    <Grid container spacing={3} style={{marginTop:'30px'}}>
+                    <Grid item xs={12} lg={4}>
+                       <Card >
   <Card.Img variant="top" src={eventezia} />
-  <Card.Body>
-    <Card.Title style={{color:'#AD365C'}}>Eventezia </Card.Title>
+  <Card.Body style={{textAlign:'center'}}>
+    <Card.Title style={{Gridor:'#AD365C'}}>Eventezia </Card.Title>
     <Card.Text>
     This popular event course guides you through each step so you become a certified event planner!
     </Card.Text>
   </Card.Body>
 </Card>
-                       </Col>
-                       <Col md={4}>
-                       <Card style={{ width: '20rem' }}>
+                       </Grid>
+                       <Grid item xs={12} lg={4}>
+                       <Card >
   <Card.Img variant="top"  src={neow} />
-  <Card.Body>
-    <Card.Title style={{color:'#AD365C'}}>The Neow </Card.Title>
+  <Card.Body style={{textAlign:'center'}}>
+    <Card.Title style={{Gridor:'#AD365C'}}>The Neow </Card.Title>
     <Card.Text>
     This popular event course guides you through each step so you become a certified event planner!
     </Card.Text>
   </Card.Body>
 </Card>
-                       </Col>
-                       <Col md={4}>
-                       <Card style={{ width: '20rem' }}>
+                       </Grid>
+                       <Grid item xs={12} lg={4}>
+                       <Card >
   <Card.Img variant="top" src={miraculous} />
-  <Card.Body>
-    <Card.Title style={{color:'#AD365C'}}>The Miraculous </Card.Title>
+  <Card.Body style={{textAlign:'center'}}>
+    <Card.Title style={{Gridor:'#AD365C'}}>The Miraculous </Card.Title>
     <Card.Text>
     This popular event course guides you through each step so you become a certified event planner!
     </Card.Text>
   </Card.Body>
 </Card>
-                       </Col>
-                    </Row>
+                       </Grid>
+                    </Grid>
                     
                 </Container>
                 <Grid container style={{marginTop:'20px'}}>
